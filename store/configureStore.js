838440024eac94b17f223c3d7187589b3066c8ca
Rodrigo Kumpera <kumpera@gmail.com>
2020-04-06 17:28:08 -0400

    Fix merge

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,17 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../rootReducer.js';
-import {
-  BleManager,
-  BleError
-} from 'react-native-ble-plx';
-
-const DeviceManager = new BleManager();
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
-    initialState,
-    applyMiddleware(thunk.withExtraArgument(DeviceManager)),
+    initialState
   );
 }
