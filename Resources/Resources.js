b89e606ca153b39350b88da4232a0636e7286420
Cheng <chengtan2013@gmail.com>
2020-04-05 21:02:38 -0400

    Resources

--- Resources/Resources.js
+++ Resources/Resources.js
@@ -1,13 +1,37 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import colors from '../assets/colors';
 
 class Resources extends Component {
   render() {
     return (
-      <Text>Resources</Text>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <Text style={styles.title}>Resources</Text>
+        </View>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: colors.PURPLE_50,
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: '500',
+  },
+});
 
 export default Resources;
