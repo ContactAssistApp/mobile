f4c38d2c4334ed92f3f75ad90b7e3455e22aff86
Cheng <chengtan2013@gmail.com>
2020-04-06 19:28:55 -0400

    Report BLE

--- Health/Report.js
+++ Health/Report.js
@@ -7,10 +7,67 @@ import {
   TouchableOpacity,
   View,
   Image,
+  NativeModules,
 } from 'react-native';
 import colors from '../assets/colors';
+import {REPORT_BLE_URL} from '../utils/endpoints';
 
 class Report extends Component {
+  uploadBLE = () => {
+    let seeds = [];
+    this.getDeviceSeedAndRotate().then(data => {
+      seeds.push(data);
+      this.reportBLE(seeds);
+    });
+  };
+
+  reportBLE = seeds => {
+    const requestBody = {
+      "seeds": seeds,
+      "region": {
+        "latitudePrefix": 74.12,
+        "longitudePrefix": -39.12,
+        "precision": 4
+      }
+    };
+
+    console.log(requestBody);
+    fetch(REPORT_BLE_URL, {
+      method: 'PUT',
+      headers: {
+        'Ocp-Apim-Subscription-Key': '037bcd2a220b4f45a023e521fc129ef2',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then(response => {
+        if (!response.ok) {
+          response.json().then(err => {
+            console.log(err);
+            return;
+          });
+        }
+        return;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  getDeviceSeedAndRotate = () => {
+    //14 days ago
+    return NativeModules.BLE.getDeviceSeedAndRotate(24 * 14 * 3600).then(
+      result => {
+        console.log('I got: ' + JSON.stringify(result));
+        return result;
+      },
+      error => {
+        console.log('failed eth: ' + error);
+      },
+    );
+  };
+
   render() {
     return (
       <SafeAreaView>
@@ -24,8 +81,7 @@ class Report extends Component {
           </Text>
           <TouchableOpacity
             style={styles.upload_trace_button}
-            onPress={() => {}}
-          >
+            onPress={this.uploadBLE}>
             <Text style={styles.upload_trace_button_text}>
               Upload Trace Data
             </Text>
---
+++ utils/endpoints.js
@@ -0,0 +1,7 @@
+export const REPORT_BLE_URL = 'https://covidsafe.azure-api.net/api/Messages/SeedReport';
+
+export const GET_QUERY_SIZE_URL = 'https://covidsafe.azure-api.net/api/Messages/Size';
+
+export const GET_MESSAGE_IDS_URL = 'https://covidsafe.azure-api.net/api/Messages/List';
+
+export const FETCH_MESSAGE_INFO_URL = 'https://covidsafe.azure-api.net/api/Message';
