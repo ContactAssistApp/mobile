d47abd9343b8b2732e57d4fb958960e6c046dcf0
Rodrigo Kumpera <kumpera@gmail.com>
2020-04-09 15:47:39 -0400

    hide under __DEV__

--- ble/ble.js
+++ ble/ble.js
@@ -14,13 +14,18 @@ const Ble = {
             );
         }
 
+        config_opts = {}
+        if(__DEV__) {
+            config_opts = {
+                "DebugLog": "yes",
+                "FastDevScan": "yes"
+            };
+        }
+
         NativeModules.BLE.init_module(
             '8cf0282e-d80f-4eb7-a197-e3e0f965848d', //service ID
             'd945590b-5b09-4144-ace7-4063f95bd0bb', //characteristic ID
-            {
-                "DebugLog": "yes",
-                "FastDevScan": "yes"
-            }
+            config_opts
         );
     },
 
