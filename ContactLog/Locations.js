09288242bf8465af418261cf8c772d7c05cb3a41
Cheng <chengtan2013@gmail.com>
2020-04-25 14:39:27 -0400

    WIP: Connect with location api

--- ContactLog/Locations.js
+++ ContactLog/Locations.js
@@ -9,6 +9,31 @@ import {
 import colors from '../assets/colors';
 
 class Locations extends Component {
+  componentDidMount() {
+    const locations = [
+      {
+        latitude: 50.934430,
+        longitude: -102.816690,
+        time: 1587843741483,
+      },
+      {
+        latitude: 50.934430,
+        longitude: -102.816690,
+        time: 1587843793871,
+      },
+      {
+        latitude: 50.934430,
+        longitude: -102.816690,
+        time: 1587843806886,
+      },
+      {
+        latitude: 40.742050,
+        longitude: -73.993851,
+        time: 1587843813376,
+      }];
+
+      // TODO: call get address method
+  }
   render() {
     return (
       <>
---
+++ ios/locations.swift
@@ -0,0 +1,9 @@
+//
+//  locations.swift
+//  covidsafe
+//
+//  Created by Cheng Tan on 4/25/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
