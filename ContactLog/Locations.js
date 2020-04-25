3f0d440089b6eea0e0dfbfb38e8c72c1345da140
Cheng <chengtan2013@gmail.com>
2020-04-25 10:22:32 -0400

    Contact log

---
+++ ContactLog/Locations.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Text,
+  Image,
+} from 'react-native';
+import colors from '../assets/colors';
+
+class Locations extends Component {
+  render() {
+    return (
+      <>
+        <Text>location</Text>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default Locations;
