2b09bb619ac3d47fba58712fa91a15ac6f3ee8ac
Cheng <chengtan2013@gmail.com>
2020-04-25 09:56:14 -0400

    Settings

---
+++ Settings/SettingLink.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Linking, TouchableOpacity} from 'react-native';
+import CustomIcon from '../assets/icons/CustomIcon.js';
+import PropTypes from 'prop-types';
+import colors from '../assets/colors';
+
+class SettingLink extends Component {
+  render() {
+    const {iconName, title, url} = this.props;
+
+    return (
+      <TouchableOpacity
+        style={styles.row}
+        onPress={() => {Linking.openURL(url)}}>
+        {
+          iconName && (
+            <CustomIcon
+              name={iconName}
+              color={colors.gray_icon}
+              size={24}
+              style={styles.icon}
+            />
+          )
+        }
+
+        <View style={styles.content}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <CustomIcon
+          name={'chevron24'}
+          color={colors.gray_icon}
+          size={24}
+        />
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: colors.card_border,
+  },
+  icon: {
+    flex: 1,
+    paddingRight: 15,
+  },
+  content: {
+    flex: 11,
+  },
+  toggle: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 17,
+    lineHeight: 22,
+    letterSpacing: -0.408,
+    color: colors.body_copy,
+    paddingBottom: 5,
+  },
+  description: {
+    fontSize: 15,
+    lineHeight: 20,
+    letterSpacing: -0.24,
+    color: colors.secondary_body_copy,
+  },
+});
+
+SettingLink.propTypes = {
+  iconName: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string,
+};
+
+export default SettingLink;
--- Settings/SettingsModal.js
+++ Settings/SettingsModal.js
@@ -3,15 +3,12 @@ import Notification from './Notification';
 import Location from './Location';
 import Bluetooth from './Bluetooth';
 import DataStorage from './DataStorage';
-import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import CustomIcon from '../assets/icons/CustomIcon.js';
 import colors from '../assets/colors';
 import Modal from '../views/Modal';
+import SettingLink from './SettingLink';
+import {UW_URL} from '../utils/constants.js';
 
 class SettingsModal extends Component {
   constructor() {
@@ -34,10 +31,39 @@ class SettingsModal extends Component {
           visible={this.state.modalOn}
           handleModalClose={this.handleModalClose}
           title={'Settings'}>
-          <Notification />
-          <Location />
-          <Bluetooth />
-          <DataStorage />
+          <>
+            <Text style={styles.section_title}>Tracing</Text>
+            <Notification />
+            <Location />
+            <Bluetooth />
+            <DataStorage />
+          </>
+
+          <>
+            <Text style={styles.section_title}>More</Text>
+            <SettingLink
+              iconName={'share24'}
+              title={'Share'}
+            />
+            <SettingLink
+              iconName={'logo24'}
+              title={'About CovidSafe'}
+              url={UW_URL}
+            />
+            <SettingLink
+              iconName={'readingMode24'}
+              title={'FAQ'}
+              url={UW_URL}
+            />
+          </>
+
+          <>
+            <Text style={styles.section_title}>Legal</Text>
+            <SettingLink
+              title={'Privacy'}
+              url={UW_URL}
+            />
+          </>
         </Modal>
 
         <TouchableOpacity onPress={() => this.setState({modalOn: true})}>
@@ -51,5 +77,16 @@ class SettingsModal extends Component {
     );
   }
 }
-
+const styles = StyleSheet.create({
+  section_title: {
+    paddingLeft: 16,
+    paddingBottom: 8,
+    paddingTop: 24,
+    fontSize: 13,
+    lineHeight: 18,
+    letterSpacing: -0.078,
+    color: colors.gray_icon,
+    backgroundColor: colors.card_border,
+  },
+});
 export default SettingsModal;
