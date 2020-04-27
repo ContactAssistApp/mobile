7c9b87da626d0952b11207e93a8a6550fbf691c5
ApolloZhu <public-apollonian@outlook.com>
2020-04-27 01:24:30 -0400

    Cache date formatter

--- ios/Locations.swift
+++ ios/Locations.swift
@@ -70,22 +70,56 @@ class Locations: NSObject {
     }
   }
 
+  // MARK: - Formatting Time
+
   /// UTC time converter
   func UTC_Converter(unixtime1: Double, unixtime2: Double, timezone: String? = nil) -> String {
     // Localization
-    var localTimeZoneAbbreviation: String { return TimeZone.current.abbreviation() ?? "" } // "UTC-4"
     let date1 = Date(timeIntervalSince1970: unixtime1)
     let date2 = Date(timeIntervalSince1970: unixtime2)
-    let dateFormatter = DateFormatter()
-    // Set timezone that you want: i.e. New York is "UTC-4"
-    dateFormatter.timeZone = TimeZone(abbreviation: timezone ?? localTimeZoneAbbreviation)
-    dateFormatter.locale = NSLocale.current
-    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss" // Specify format that you want
+    let dateFormatter = makeDateFormatter(inTimezone: timezone)
     let strDate1 = dateFormatter.string(from: date1)
     let strDate2 = dateFormatter.string(from: date2)
     return (strDate1 + "~" + strDate2)
   }
-  
+
+  /// It's actually an expensive operation to create date formatters.
+  private func makeDateFormatter(inTimezone timezone: String?) -> DateFormatter {
+    guard let timezone = timezone else { return formatter }
+    let dateFormatter = DateFormatter()
+    // Set timezone that you want: i.e. New York is "UTC-4"
+    dateFormatter.timeZone = TimeZone(abbreviation: timezone)
+    dateFormatter.locale = NSLocale.current
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss" // Specify format that you want
+    return dateFormatter
+  }
+
+  /// Therefore caches the formatter for default (current) timezone
+  private lazy var formatter: DateFormatter = {
+    // e.g. "UTC-4"
+    return makeDateFormatter(inTimezone: localTimeZoneAbbreviation)
+  }()
+
+  @objc
+  private func updateDefaultFormatter() {
+    formatter = makeDateFormatter(inTimezone: localTimeZoneAbbreviation)
+  }
+
+  private var localTimeZoneAbbreviation: String {
+    return TimeZone.current.abbreviation() ?? ""
+  }
+
+  override init() {
+    super.init()
+    /// Updates date formatter when timezone changes
+    NotificationCenter.default
+      .addObserver(self, selector: #selector(updateDefaultFormatter),
+                   name: .NSSystemTimeZoneDidChange, object: nil)
+  }
+
+  deinit {
+    NotificationCenter.default.removeObserver(self)
+  }
 
   //---------------------------------------------------------------------------------
   // MARK: - Step2: aggregate address result - from timestamp to period
