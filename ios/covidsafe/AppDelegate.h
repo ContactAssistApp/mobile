114b62ceb9937067500c556bfcef70f30b4f7405
ApolloZhu <public-apollonian@outlook.com>
2020-04-28 16:24:44 -0400

    Remove "Inc and affiliates"

--- ios/covidsafe/AppDelegate.h
+++ ios/covidsafe/AppDelegate.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) CovidSafe, Inc. and its affiliates.
+ * Copyright (c) CovidSafe.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -10,6 +10,6 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@property (nonatomic, strong) UIWindow *window;
+@property(nonatomic, strong) UIWindow *window;
 
 @end
