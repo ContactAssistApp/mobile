37704fdb5e7aa0bd94c02e5058eba23f6a0cb278
Cheng <chengtan2013@gmail.com>
2020-04-28 15:56:29 -0400

    Pull in contact list

--- ContactLog/People.js
+++ ContactLog/People.js
@@ -1,8 +1,33 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text} from 'react-native';
 import colors from '../assets/colors';
+import Contacts from 'react-native-contacts';
+import {TouchableOpacity} from 'react-native';
+import CustomIcon from '../assets/icons/CustomIcon.js';
 
 class People extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contactList: [],
+    };
+  }
+
+  loadContact = () => {
+    Contacts.getAll((err, contacts) => {
+      if (err) {
+        throw err;
+      }
+      const contactList = contacts.map(contact => {
+        return `${contact.givenName} ${contact.familyName}`;
+      });
+
+      this.setState({
+        contactList,
+      });
+    });
+  };
+
   render() {
     return (
       <>
@@ -10,6 +35,9 @@ class People extends Component {
         <Text style={styles.description}>
           Safely add people you’ve been in contact with directly from your contacts list, or one at a time.
         </Text>
+        <TouchableOpacity onPress={this.loadContact} style={styles.add_button}>
+          <CustomIcon name={'add24'} color={'white'} size={20} />
+        </TouchableOpacity>
       </>
     );
   }
@@ -30,6 +58,17 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: 'white',
   },
+  add_button: {
+    backgroundColor: colors.primary_theme,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  }
 });
 
 export default People;
