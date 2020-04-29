9a47005d63da2f1c9f8e2388b290a497461bdfd2
Cheng <chengtan2013@gmail.com>
2020-04-29 19:00:09 -0400

    InterviewPrep pulls in 14 days symptoms

---
+++ InterviewPrep/DateHeader.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import colors from '../assets/colors';
+import DateConverter from '../utils/date';
+
+class DateHeader extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.date}>
+          {DateConverter.dateString(new Date(this.props.date))}
+          {this.props.timeOfDay && (
+            <Text style={styles.time}> {this.props.timeOfDay}</Text>
+          )}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingHorizontal: 19,
+    paddingVertical: 11,
+    borderBottomColor: colors.card_border,
+    borderBottomWidth: 1,
+  },
+  date: {
+    fontSize: 14,
+    lineHeight: 18,
+    textTransform: 'capitalize',
+    color: colors.secondary_body_copy,
+  },
+  time: {
+    textTransform: 'uppercase',
+  },
+});
+
+export default DateHeader;
---
+++ InterviewPrep/SectionHeader.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text} from 'react-native';
+import colors from '../assets/colors';
+
+class SectionHeader extends Component {
+  render() {
+    return <Text style={styles.header}>{this.props.header}</Text>;
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {
+    fontWeight: '500',
+    fontSize: 14,
+    lineHeight: 18,
+    textTransform: 'uppercase',
+    color: colors.secondary_body_copy,
+    padding: 20,
+    backgroundColor: colors.card_border,
+  },
+});
+
+export default SectionHeader;
