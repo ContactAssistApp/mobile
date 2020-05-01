e166dc982d98e0e72c8014b36fe20f441eaa0c50
ApolloZhu <public-apollonian@outlook.com>
2020-05-01 02:40:36 -0400

    Process KML

--- Settings/ImportGoogleTimeline.js
+++ Settings/ImportGoogleTimeline.js
@@ -1,21 +1,93 @@
 import React, {Component} from 'react';
 import {View, StyleSheet, Text, TouchableOpacity, Alert} from 'react-native';
-import {GetStoreData} from '../utils/asyncStorage';
+import {parseString} from 'react-native-xml2js';
 import Modal from '../views/Modal';
 import Icon from 'react-native-vector-icons/Ionicons';
 import colors from '../assets/colors';
-import GoogleTimelineImportView from './GoogleTimelineImportView.js';
+import GoogleTimelineImportView from './GoogleTimelineImportView';
 
 class ImportGoogleTimeline extends Component {
   constructor() {
     super();
     this.state = {
       googleSignInVisible: false,
+      title: 'Sign-in to Google',
     };
   }
 
   handleModalClose = () => {
     this.setState({googleSignInVisible: false});
+  };
+
+  importFinished = (reason, notFailure) => {
+    let title, description;
+    if (notFailure) {
+      title = reason;
+    } else {
+      title = 'Failed to Import Location from Google';
+      description = reason;
+    }
+    Alert.alert(title, description, [
+      {
+        onPress: this.handleModalClose,
+      },
+    ]);
+  };
+
+  processPlacemarks = placemarks => {
+    let results = [];
+    function push(coordinates, time) {
+      let [long, lat] = coordinates.split(',');
+      results.push({
+        latitude: lat,
+        longitude: long,
+        time: time,
+      });
+    }
+    for (const placemark of placemarks) {
+      let timeStart = Date.parse(placemark.TimeSpan[0].begin[0]);
+      let timeEnd = Date.parse(placemark.TimeSpan[0].end[0]);
+      let point = placemark.Point;
+      if (point) {
+        let coordinates = point[0].coordinates[0];
+        push(coordinates, timeStart);
+        push(coordinates, timeEnd);
+      } else {
+        let allCoordinates = placemark.LineString[0].coordinates[0]
+          .trim()
+          .split(' ');
+        let timeIncrement = (timeEnd - timeStart) / (allCoordinates.length - 1);
+        let currentTime = timeStart;
+        for (let coordinates of allCoordinates) {
+          push(coordinates, Math.round(currentTime));
+          currentTime += timeIncrement;
+        }
+      }
+    }
+    console.log(results);
+  };
+
+  processKMLResponse = dict => {
+    this.setState({title: 'Importing Location History'});
+    if (dict.data) {
+      parseString(dict.data, (error, result) => {
+        if (result) {
+          let placemarks = result.kml.Document[0].Placemark;
+          if (placemarks && placemarks.length > 0) {
+            this.processPlacemarks(placemarks);
+          } else {
+            this.importFinished(
+              "You don't have any location history on Google.",
+              true,
+            );
+          }
+        } else {
+          this.importFinished(error);
+        }
+      });
+    } else {
+      this.importFinished(dict.error);
+    }
   };
 
   render() {
@@ -25,27 +97,10 @@ class ImportGoogleTimeline extends Component {
           visible={this.state.googleSignInVisible}
           handleModalClose={this.handleModalClose}
           useScrollView={false}
-          title={'Sign-in to Google'}>
+          title={this.state.title}>
           <GoogleTimelineImportView
             style={styles.wrapper}
-            onReceivingPlacemarks={dict => {
-              console.log(dict);
-              if (dict.data) {
-                /*
-              And the parser extracts the start and end location, timestamp, and mode of transportation of every entry
-              */
-              } else {
-                Alert.alert(
-                  'Failed to Import Location from Google',
-                  dict.error,
-                  [
-                    {
-                      onPress: this.handleModalClose,
-                    },
-                  ],
-                );
-              }
-            }}
+            onReceivingPlacemarks={this.processKMLResponse}
           />
         </Modal>
         <View style={styles.row}>
