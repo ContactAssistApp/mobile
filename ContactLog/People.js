a82fc50ffb24a4b6e6bb5d24954ec2020c746ce5
Cheng <chengtan2013@gmail.com>
2020-05-21 08:49:15 -0400

    Fix interview prep formatting

--- ContactLog/People.js
+++ ContactLog/People.js
@@ -11,8 +11,7 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import SelectedContacts from './SelectedContacts';
-import { strings } from '../locales/i18n';
-
+import {strings} from '../locales/i18n';
 
 class People extends Component {
   constructor() {
