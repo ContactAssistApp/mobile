3be74af78a7def5895fc948b7c95af72066f3835
Rodrigo Kumpera <kumpera@gmail.com>
2020-05-26 14:18:57 -0400

    Remove BLE backend.

--- ios/BeaconBroadcast.h
+++
@@ -1,7 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-#import <CoreBluetooth/CoreBluetooth.h>
-#import <CoreLocation/CoreLocation.h>
-
-@interface BeaconBroadcast : NSObject <RCTBridgeModule, CLLocationManagerDelegate, CBPeripheralManagerDelegate>
-@end
