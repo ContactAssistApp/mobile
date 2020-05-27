77174ee18bb7c7545d1baef899f14228c49a64c9
Cheng <chengtan2013@gmail.com>
2020-05-27 10:40:38 -0400

    Date fix

--- Health/Symptoms.js
+++ Health/Symptoms.js
@@ -1,24 +1,22 @@
 import React, {Component} from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import SymptomTracker from '../SymptomTracker/SymptomTracker';
-import DateConverter from '../utils/date';
 import CareTips from '../CareTips/CareTips';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getDaysWithLog} from '../realm/realmSymptomsTasks';
 import {updateHealthData} from './actions.js';
 import PropTypes from 'prop-types';
-import { strings, fmt_date } from '../locales/i18n';
-
+import {fmt_date} from '../locales/i18n';
 
 class Symptoms extends Component {
   componentDidMount() {
     this.fetchMarkedDays();
   }
 
-  fetchMarkedDays = () => {
+  fetchMarkedDays = async () => {
     let markedDates = {};
-    const dates = getDaysWithLog();
+    const dates = await getDaysWithLog();
 
     dates.forEach(date => {
       markedDates[date] = {marked: true};
