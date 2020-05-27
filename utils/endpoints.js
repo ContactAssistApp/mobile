859996472eba21052e7d1b14bef17ae296f05736
Rodrigo Kumpera <kumpera@gmail.com>
2020-05-27 13:29:49 -0400

    Remove BLE reporting.

--- utils/endpoints.js
+++ utils/endpoints.js
@@ -1,5 +1,3 @@
-export const REPORT_BLE_URL = 'https://csapi.azurefd.net/api/Messages/SeedReport';
-
 export const GET_MESSAGE_LIST_URL = 'https://csapi.azurefd.net/api/Messages/List';
 
 export const FETCH_MESSAGE_INFO_URL = 'https://csapi.azurefd.net/api/Message';
