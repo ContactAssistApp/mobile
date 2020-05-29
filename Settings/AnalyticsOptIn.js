81a83c06db1796855101d9655ac2d8e206266eb6
Rodrigo Kumpera <kumpera@gmail.com>
2020-05-29 17:53:39 -0400

    Remove Settings which is iOS only and implement appcenter optin.

--- Settings/AnalyticsOptIn.js
+++ Settings/AnalyticsOptIn.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react';
-import {Settings} from 'react-native';
 import TraceTool from './TraceTool';
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
+import {GetStoreData, SetStoreData} from '../utils/asyncStorage';
 
 class AnalyticsOptIn extends Component {
   constructor(props) {
     super();
     this.state = {
-      optIn: !!Settings.get('analyticsOptIn'),
-    };
+      optIn: false
+    }
   }
 
+  componentDidMount() {
+    this.getSetting('ANALYTICS-OPTIN').then(data => {
+      this.setState( { optIn: data })
+    });
+  }
+
+  getSetting = async (key) => {
+    let data = await GetStoreData(key);
+    return data === 'true' ? true : false;
+  };
+
+
   updateSetting = state => {
-    Settings.set({'analyticsOptIn': state});
+    let changeIt = async() => {
+      await Crashes.setEnabled(state);
+      await Analytics.setEnabled(state);
+      await SetStoreData('ANALYTICS-OPTIN', state)
+    };
+
+    changeIt().catch(reason => console.log(reason));
 
     this.setState({
       optIn: state,
