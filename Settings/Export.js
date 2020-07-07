12910cd27912c71b8d4df2f6482c6966fd03b7b7
Rodrigo Kumpera <kumpera@gmail.com>
2020-07-07 14:40:46 -0400

    rework export to work on iOS. Android still can't do file uploads.

--- Settings/Export.js
+++ Settings/Export.js
@@ -1,42 +1,14 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity, Platform} from 'react-native';
 import colors from '../assets/colors';
 import CustomIcon from '../assets/icons/CustomIcon.js';
 import {strings} from '../locales/i18n';
 import Share from "react-native-share";
 import RealmObj from '../realm/realm';
-const base64js = require('base64-js');
+import * as RNFS from 'react-native-fs';
 
-function toUTF8Array(str) {
-  let utf8 = [];
-  for (let i = 0; i < str.length; i++) {
-      let charcode = str.charCodeAt(i);
-      if (charcode < 0x80) utf8.push(charcode);
-      else if (charcode < 0x800) {
-          utf8.push(0xc0 | (charcode >> 6),
-                    0x80 | (charcode & 0x3f));
-      }
-      else if (charcode < 0xd800 || charcode >= 0xe000) {
-          utf8.push(0xe0 | (charcode >> 12),
-                    0x80 | ((charcode>>6) & 0x3f),
-                    0x80 | (charcode & 0x3f));
-      }
-      // surrogate pair
-      else {
-          i++;
-          // UTF-16 encodes 0x10000-0x10FFFF by
-          // subtracting 0x10000 and splitting the
-          // 20 bits of 0x0-0xFFFFF into two halves
-          charcode = 0x10000 + (((charcode & 0x3ff)<<10)
-                    | (str.charCodeAt(i) & 0x3ff));
-          utf8.push(0xf0 | (charcode >>18),
-                    0x80 | ((charcode>>12) & 0x3f),
-                    0x80 | ((charcode>>6) & 0x3f),
-                    0x80 | (charcode & 0x3f));
-      }
-  }
-  return utf8;
-}
+const base64js = require('base64-js');
+const Buffer = require("buffer").Buffer;
 
 class Export extends Component {
   constructor() {
@@ -51,22 +23,46 @@ class Export extends Component {
       'area-matches': real.objects('AreaMatches'),
       'locations': real.objects('Location'),
       'symptoms': real.objects('Symptoms')
-    }
-    let payload = base64js.fromByteArray(toUTF8Array(JSON.stringify(dump)));
-    const options = {
-      title: strings('export.message_title'),
-      subject: strings('export.subject'),
-      message: strings('export.message'),
-      filename: 'common-circle-dump',
-      url: 'data:text/plain;base64,' + payload,
-      failOnCancel: false
     };
-    Share.open(options)
-      .then((res) => {
-        console.log("export ok:" + JSON.stringify(res))
-      }).catch((err) => {
-        console.log("export failed:" + JSON.stringify(err))
+    try {
+      let the_url = null;
+
+      if(Platform.OS == 'android') {
+        let encodedData = new Buffer(JSON.stringify(dump)).toString("base64");
+        the_url = 'data:text/plain;base64,' + encodedData;
+      } else {
+        let path = RNFS.DocumentDirectoryPath + "/dump.json";
+        try {
+          await RNFS.unlink(path);
+        } catch(e) {
+          // unlink fails if the file doesn't exist, which is fine
+        }
+        
+        await RNFS.writeFile(path, JSON.stringify(dump), 'utf8');
+        the_url = 'file://' + path;
+      }
+      console.log('dump created successfully');  
+
+      let res = await Share.open( {
+        url: the_url,
+        filename: 'common-circle-dump',
+          message: strings('export.message'),
+        failOnCancel: false
       });
+      console.log('sharing ok:' + JSON.stringify(re));
+    } catch (e) {
+      console.log('sharing failed due to: ' + JSON.stringify(e));
+    }
+
+    //delete the file regardless of what happened
+    if(Platform.OS != 'android') {
+      try {
+        console.log('removing file');
+        await RNFS.unlink(path);
+      } catch(e) {
+        // unlink fails if the file doesn't exist, which is fine
+      }
+    }
   }
 
   render() {
