efe7868576208486dae0c329cf1351d5ab790035
Rodrigo Kumpera <kumpera@users.noreply.github.com>
2020-07-16 17:35:48 -0400

    Gps improvements2 (#313)
    * merge stuff.
    
    * BackgrounGEO improvements. Ask for higher resolution, log errors and switch to the Activity provider.

--- utils/location.js
+++ utils/location.js
@@ -1,6 +1,9 @@
 import {NativeModules} from 'react-native';
 import {getLocations} from '../realm/realmLocationTasks';
 
+//minimum accuracy of a location for us to care for
+const MIN_ACCURACY = 100
+
 function distance(lat1, lon1, lat2, lon2) {
   if (lat1 === lat2 && lon1 === lon2) {
     return 0;
@@ -29,19 +32,70 @@ const Location = {
   fetchAddresses: async function(dateObj, dayRange = 0) {
     const locations = await getLocations(dateObj, dayRange);
 
-    const addresses = locations.filter((location, index, self) => {
-      if(location.kind == 'moving' && location.speed > 1)
-        return false;
-      return index === self.findIndex(t => t.address === location.address);
+    let candidates_dict = {}
+    let possible_locs = []
+    locations.forEach(loc => {
+      //ignore unknown locations
+      if(loc.name == 'Unknown')
+        return;
+      //ignore non-stationary locations 
+      if(loc.speed > 1)
+        return;
+      //ignore low precision data points
+      if(loc.accuracy > MIN_ACCURACY)
+        return;
+      if(!candidates_dict[loc.address])
+      {  let g = {
+          latitude: loc.latitude,
+          longitude: loc.longitude,
+          accuracy: loc.accuracy,
+          name: loc.name,
+          address: loc.address,
+          count: 1,
+          valid: true
+        };
+        candidates_dict[loc.address] = g;
+        possible_locs.push(g);
+      } else {
+        let g = candidates_dict[loc.address];
+        g.latitude += loc.latitude;
+        g.longitude += loc.longitude;
+        g.accuracy += loc.accuracy;
+        g.count++;
+      }
+    });
+
+    possible_locs.forEach(location => {
+      location.latitude /= location.count;
+      location.longitude /= location.count;
     })
-    .map(location => {
+
+    for (var i = 0; i < possible_locs.length; ++i) {
+      var src = possible_locs[i];
+      if(!src.valid)
+        continue;
+      for (var j = i + 1; j < possible_locs.length; ++j) {
+        var dest = possible_locs[j]
+        if(!dest.valid)
+          continue;
+        if(distance(src.latitude, src.longitude, dest.latitude, dest.longitude) < (src.accuracy + dest.accuracy) / 2) {
+          if(src.count > dest.count) {
+            dest.valid = false;
+            src.count += dest.count;
+          } else {
+            src.valid = false;
+            dest.count += src.count;
+          }
+        }
+      }
+    }
+
+    return possible_locs.map(location => {
       return {
         name: location.name,
         address: location.address,
       }
     });
-
-    return addresses;
   },
   isAreaMatch: async function(area) {
     const {
