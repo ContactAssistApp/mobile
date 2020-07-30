6a474bca8c9e636e250d03ea2c163639d2d6b3a5
Hung Ngo <hung.ngovinh97@gmail.com>
2020-07-30 15:57:53 -0700

    Configure redux debugger

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,7 +1,11 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../rootReducer.js';
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, applyMiddleware(thunk));
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(rootReducer, initialState,
+    composeEnhancers(applyMiddleware(thunk))
+  );
 }
